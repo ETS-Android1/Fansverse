ddb00d0313f021be3ffa89354180bf0f9d9cb626
Jovanhy <jose.j.uriarte@gmail.com>
2022-02-11 18:37:59 -0800

    logout btn added to homepage
    logout btn will successfully logout user from app and direct them to the login screen

--- CreatingUser/app/src/main/java/com/example/creatinguser/HomePage.java
+++ CreatingUser/app/src/main/java/com/example/creatinguser/HomePage.java
@@ -6,8 +6,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class HomePage extends AppCompatActivity {
 
@@ -17,6 +20,7 @@ public class HomePage extends AppCompatActivity {
     private Button stats;
     private Button myProfile;
     Button btnNewsfeed;
+    private Button logout_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,12 +33,24 @@ public class HomePage extends AppCompatActivity {
         stats = findViewById(R.id.buttonStats);
         myProfile = findViewById(R.id.buttonProfile);
         btnNewsfeed = findViewById(R.id.btnNewsfeed);
+        logout_button = findViewById(R.id.logout_button);
 
         btnNewsfeed.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), Newsfeed.class);
                 //intent.putExtra("message", current_username);
+                startActivity(intent);
+            }
+        });
+
+        logout_button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().getCurrentUser();
+                FirebaseAuth.getInstance().signOut();
+                Toast toast = Toast.makeText(HomePage.this, "Signout Complete", Toast.LENGTH_LONG);
+                toast.show();
+                Intent intent = new Intent(getApplicationContext(), LoginPage.class);
                 startActivity(intent);
             }
         });
