83adccfb08d801e3497718241097cf7ed3381c0c
Dmitry-057 <merezhko.dmitry@gmail.com>
2022-03-22 16:39:28 -0700

    Setting up gallery

---
+++ CreatingUser/app/src/main/java/com/example/creatinguser/LocationGallery.java
@@ -0,0 +1,14 @@
+package com.example.creatinguser;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LocationGallery extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_location_gallery);
+    }
+}
