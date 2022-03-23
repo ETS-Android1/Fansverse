afc909a9f09d6b9a49eed59254b56aae8795d317
Dmitry-057 <merezhko.dmitry@gmail.com>
2022-03-22 18:01:51 -0700

    More work on gallery

--- CreatingUser/app/src/main/java/com/example/creatinguser/LocationGallery.java
+++ CreatingUser/app/src/main/java/com/example/creatinguser/LocationGallery.java
@@ -1,8 +1,18 @@
 package com.example.creatinguser;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 
 public class LocationGallery extends AppCompatActivity {
 
@@ -10,5 +20,44 @@ public class LocationGallery extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location_gallery);
+
+        // getting ImageView by its id
+        ImageView rImage = findViewById(R.id.rImage);
+
+        // we will get the default FirebaseDatabase instance
+        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+
+        // we will get a DatabaseReference for the database root node
+        DatabaseReference databaseReference = firebaseDatabase.getReference();
+
+        // Here "image" is the child node value we are getting
+        // child node data in the getImage variable
+        DatabaseReference getImage = databaseReference.child("Interlude1");
+
+        // Adding listener for a single change
+        // in the data at this location.
+        // this listener will triggered once
+        // with the value of the data at the location
+        getImage.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                // getting a DataSnapshot for the location at the specified
+                // relative path and getting in the link variable
+                String link = dataSnapshot.getValue(String.class);
+
+                // loading that data into rImage
+                // variable which is ImageView
+                Picasso.get().load(link).into(rImage);
+            }
+
+            // this will called when any problem
+            // occurs in getting data
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // we are showing that error message in toast
+                Toast.makeText(LocationGallery.this, "Error Loading Image", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
